53eae46f72910e0399f187e6e0da66bc9a4f8246
kraa <kraa@omegafuture.ru>
2024-03-21 15:40:14 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {Provider} from "react-redux";
-import {store} from "./store/store.ts";
 import {QueryClientProvider} from "react-query";
-import {queryClient} from "./api/api.ts";
+import {QueryClient} from "react-query";
+
+const queryClient=new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
       <QueryClientProvider client={queryClient}> {/* Используем QueryClientProvider и передаем в него QueryClient */}
         <App />
       </QueryClientProvider>
-    </Provider>
   </React.StrictMode>,
 )
